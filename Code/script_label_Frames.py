c83de14338724e5261c823c38e3e7610c6e8b97b
jeux19 <jesgarram19@gmail.com>
2020-01-28 12:08:18 +0100

    Proccess all clips of same art piece simultaniously

--- Code/script_label_Frames.py
+++ Code/script_label_Frames.py
@@ -13,8 +13,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--main_dir", type=str,
                     help="Base directory of annotated data")
 
-parser.add_argument("--video_file", type=str,
-                    help="Filename of clip to extract frames")
+parser.add_argument("--video_folder", type=str,
+                    help="Folder containing all clips of same artwork")
 
 parser.add_argument("--table_file", type=str,
                     help="File of table with annotations")
@@ -27,17 +27,24 @@ parser.add_argument("-u", "--update", action="store_true",
 
             
 if __name__ == "__main__":
+    #Sample usage for sampling
+    #python script_label_Frames.py --main_dir "C:\Users\jeuux\Desktop\Carrera\MoAI\TFM\AnnotatedData" --video_folder "Y3A6092" -s
     args = parser.parse_args()
     #Define neccesary paths for frames manipulation
-    table_path=os.path.join(args.main_dir,args.table_file)
-    dest_dir=os.path.join(args.main_dir,"Frames")
     buffer_dir=os.path.join(args.main_dir,"Buffer")
+    video_folder=os.path.join(args.main_dir,"Clips",args.video_folder)
     if (args.sample):
-        video_folder= os.path.join(args.main_dir,"Clips",args.video_file)
-        get_samples(video_folder,buffer_dir)
+        for file in os.listdir(video_folder):
+            video_file=os.path.join(video_folder,file)
+            get_samples(video_file,buffer_dir)
     elif(args.update):
+        table_path=os.path.join(args.main_dir,args.table_file)
+        dest_dir=os.path.join(args.main_dir,"Frames")
         update_annotations(buffer_dir,table_path,dest_dir)
     else:
         print("Need to specify some valid action (Sample/Update)")
     pass
+
+
+
 
