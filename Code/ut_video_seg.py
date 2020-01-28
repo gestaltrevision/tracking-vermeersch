0c80c97f6be50cc59fe18f5256679834bbb4b898
jeux19 <jesgarram19@gmail.com>
2020-01-28 12:28:36 +0100

    Added splitting frames Train/Val

--- Code/ut_video_seg.py
+++ Code/ut_video_seg.py
@@ -30,6 +30,24 @@ def get_samples(video_file,frames_folder,nsamples=30):
             
     print("Succesfully saved frames")
     cap.release()
+
+# dataset_path=os.path.join(main_dir,"Classification_Frames_Dataset")
+def split_dataset(dataset_path,picture_class):
+    Frames_folder=os.path.join(dataset_path,picture_class)
+    Frames=[frame for frame in os.listdir(Frames_folder)] #Filenames of frames from frames_folder
+    #Split
+    Frames_train, Frames_val= train_test_split(Frames,random_state=42) #split filenames into Train/val
+    #Move actual frames using split filenames
+    for file in Frames_train: #train
+        source=os.path.join(Frames_folder,file)
+        dest=os.path.join(dataset_path,"Train",picture_class)
+        shutil.move(source,dest)
+        
+    for file in Frames_val: #val
+        source=os.path.join(Frames_folder,file)
+        dest=os.path.join(dataset_path,"Val",picture_class)
+        shutil.move(source,dest)
+        
 # #Update Table
 def update_annotations(buffer_dir,table_path,dest_dir):
     sub_df=create_subtable(buffer_dir)
