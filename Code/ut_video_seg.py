55b7da652fa87052addb14481b75b04294d12ebe
jeux19 <jesgarram19@gmail.com>
2020-01-29 17:26:24 +0100

    added calc stats dataset, split dataset

--- Code/ut_video_seg.py
+++ Code/ut_video_seg.py
@@ -4,6 +4,7 @@ import cv2
 import os
 import shutil
 from general_utilities import cd,insert_string,get_filename
+from sklearn.model_selection import train_test_split
 
 
 
@@ -32,22 +33,54 @@ def get_samples(video_file,frames_folder,nsamples=30):
     cap.release()
 
 # dataset_path=os.path.join(main_dir,"Classification_Frames_Dataset")
-def split_dataset(dataset_path,picture_class):
-    Frames_folder=os.path.join(dataset_path,picture_class)
-    Frames=[frame for frame in os.listdir(Frames_folder)] #Filenames of frames from frames_folder
-    #Split
-    Frames_train, Frames_val= train_test_split(Frames,random_state=42) #split filenames into Train/val
-    #Move actual frames using split filenames
-    for file in Frames_train: #train
-        source=os.path.join(Frames_folder,file)
-        dest=os.path.join(dataset_path,"Train",picture_class)
-        shutil.move(source,dest)
-        
-    for file in Frames_val: #val
-        source=os.path.join(Frames_folder,file)
-        dest=os.path.join(dataset_path,"Val",picture_class)
-        shutil.move(source,dest)
-        
+def split_dataset(dataset_path):
+    picture_labels=[picture for picture in os.listdir(dataset_path) if "Y" in picture]
+    for picture in picture_labels:
+        Frames_folder=os.path.join(dataset_path,picture)
+        Frames=[frame for frame in os.listdir(Frames_folder)] #Filenames of frames from frames_folder
+        #Split
+        Frames_train, Frames_val= train_test_split(Frames,random_state=42) #split filenames into Train/val
+        #Move actual frames using split filenames
+        for file in Frames_train: #train
+            source=os.path.join(Frames_folder,file)
+            dest=os.path.join(dataset_path,"Train",picture)
+            shutil.move(source,dest)
+
+        for file in Frames_val: #val
+            source=os.path.join(Frames_folder,file)
+            dest=os.path.join(dataset_path,"Val",picture)
+            shutil.move(source,dest)
+        print("Split Frames from artwork {0}".format(picture))
+    print("Dataset split with success")
+
+
+
+def statistics_dataset(dataset_path):
+    """Return mean and std of dataset, this parameters will be used to normalize img in training"""
+    mean_arr=[]
+    std_arr=[]
+
+    for folder in ["Train","Val"]:
+        folder_path=os.path.join(dataset_path,folder)
+        labels=[label for label in os.listdir(folder_path)]
+
+        for label in labels:
+            label_path=os.path.join(folder_path,label)
+            imgs=os.listdir(label_path)
+
+            for img in imgs:
+                img_path=os.path.join(label_path,img)
+                #read img [0,1]
+                image = cv2.imread(img_path).astype(np.float32) / 255
+                mean,std=cv2.meanStdDev(image)
+                mean_arr.append(mean)
+                std_arr.append(std)
+            
+    dataset_mean =np.mean(np.array(mean_arr, dtype=np.float32),axis=0)
+    dataset_std =np.mean(np.array(std_arr, dtype=np.float32),axis=0)
+
+    return dataset_mean,dataset_std
+
 # #Update Table
 def update_annotations(buffer_dir,table_path,dest_dir):
     sub_df=create_subtable(buffer_dir)
