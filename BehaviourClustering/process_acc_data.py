1ee8a2d4a83292ac07b12e9d8efaa1dde86d9291
jeux19 <jesgarram19@gmail.com>
2020-03-16 19:47:27 +0100

    scripts process acc data

--- BehaviourClustering/process_acc_data.py
+++ BehaviourClustering/process_acc_data.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 import os
 from tqdm import tqdm
-from acc_ut import get_psd_values,get_fft_values,get_autocorr_values,get_features
+import sys
+project_path=os.path.dirname(os.getcwd()) 
+sys.path.append(os.path.join(project_path,"Code"))
 from acc_ut import create_df_participant,get_participant,get_scaler
-from pickle import dump
+import json
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
@@ -14,33 +16,34 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--main_dir", type=str,
                     help="Base directory of annotated data")
+
+
+parser.add_argument("--parameters_file" ,type=str,
+                    help="path to json file containing parameter extraction info")
                     
-parser.add_argument("--scaler_name", type=str,
-                    help="Basename of file to save scaler data")
-
-
 parser.add_argument("-wd","--window_duration", type=int,
                     help="Duration of sliding window interval in secs")
-
-parser.add_argument("--starts", nargs='+', type=float,
-                    help="vector of starts to create sliding windows")
-
+                    
+parser.add_argument("-sr","--sample_rate", type=int,
+                    help="Time between samples of accelerometer in msecs")
+                    
 parser.add_argument("-d", "--default", action="store_true",
-                    help="default values for window_duration (2 secs) and starts (0s,0.5s)")
+                    help="default values for window_duration (2 secs) and sample rate(10 msecs)")
 
 
             
 if __name__ == "__main__":
     #Sample usage with default values
-    #python process_acc_data.py --main_dir "C:\Users\jeuux\Desktop\Carrera\MoAI\TFM" --scaler_name "scaler" -d
+    #python process_acc_data.py --main_dir "C:\Users\jeuux\Desktop\Carrera\MoAI\TFM" --parameters_file "C:\Users\jeuux\Desktop\Carrera\MoAI\TFM\AnnotatedData\Accelerometer_Data\Datasets\fc_parameters_basic.json" -d
     args = parser.parse_args()
     #Define neccesary paths for data manipulation
     if args.default==True:
         window_duration=2
-        starts=[0,0.5]
+        sample_rate=10
+
     elif args.default==False:
         window_duration=args.window_duration
-        starts=args.starts
+        sample_rate=args.sample_rate
 
     acc_path=os.path.join(args.main_dir,"AnnotatedData","Accelerometer_Data")
     #Getting path from acc tables
@@ -50,54 +53,52 @@ if __name__ == "__main__":
                     for file in os.listdir(os.path.join(participant_path,folder))
                     if "acc_data_" in file]
     
-    acc_table_arr=[];participant_arr=[]
-    #open raw tables to get raw acc values and participant info
-    for file in table_paths:
-        #get participant
-        participant_arr.append(get_participant(file,"acc_data_"))
-        acc_table_arr.append(pd.read_csv(file)) ##change to already processed tables
 
-    acc_table = pd.concat(acc_table_arr)
-
-    #Get means and standard deviations from full accelerometer table
-    cols=["AccX","AccY","AccZ"]
-    scaler=get_scaler(acc_table,cols) 
-    # save the scaler
     dataset_path=os.path.join(acc_path,"Datasets")
-    scaler_path=os.path.join(dataset_path,"{}.pkl".format(args.scaler_name))
-    dump(scaler, open(scaler_path, 'wb'))
 
+    #get fc_parameters
+    with open(args.parameters_file) as f:
+        fc_parameters = json.load(f)
+
+    #open raw tables to get raw acc values and participant info
+    cols=["id","time","AccX","AccY","AccZ"]
     table_arr=[]
-    for participant,df in tqdm(zip(participant_arr, acc_table_arr)):
-        #Normalize
-        x = df[cols].values
-        x_scaled = scaler.fit_transform(x)
-        df = pd.concat((df["time"],pd.DataFrame(x_scaled,columns=cols)),axis=1)
-        #Extract features
-        table_arr.append(create_df_participant(df[cols],participant,window_duration,starts))
-
+    for file in tqdm(table_paths):
+        df=pd.read_csv(file)
+        #Extract features 
+        table_arr.append(create_df_participant(df[cols],fc_parameters,window_duration,sample_rate))
     #concat tables
-    complete_df = pd.concat(table_arr)
-    
+    complete_table = pd.concat(table_arr)
     #Split train/val
-    index_list=np.asarray(complete_df.index)
+    index_list=np.asarray(complete_table.index)
     idx_train, _= train_test_split(index_list,random_state=42) #split index into Train/val
     train_val=[1 if idx in idx_train else 0 for idx in index_list]
-    complete_df["train"]=train_val #Create column with "1" if entry is a train entry, 0 otherwise
-    complete_df["target"]= np.nan
+    complete_table["train"]=train_val #Create column with "1" if entry is a train entry, 0 otherwise
+    complete_table["target"]= np.nan
     
     #split tables
-    train_df=complete_df[complete_df["train"]==1]
-    train_df=train_df.reset_index(drop=True)
-    test_df=complete_df[complete_df["train"]==0]
-    test_df=test_df.reset_index(drop=True)
+    train_df=complete_table[complete_table["train"]==1]
+    train_df=train_df.reset_index()
+    test_df=complete_table[complete_table["train"]==0]
+    test_df=test_df.reset_index()
     
     #save tables
-    test_df.to_csv(os.path.join(dataset_path,"{0}_test.csv".format("acc_dataset")),index=False)
-    train_df.to_csv(os.path.join(dataset_path,"{0}_train.csv".format("acc_dataset")),index=False)
+    test_df.to_csv(os.path.join(dataset_path,"acc_dataset_test.csv"),index=False)
+    train_df.to_csv(os.path.join(dataset_path,"acc_dataset_train.csv"),index=False)
 
     print("Succesfully created")
 
 
 
 
+
+
+        # #Get means and standard deviations from full accelerometer table
+        # scaler=get_scaler(acc_table,cols) 
+        # # save the scaler
+        # scaler_path=os.path.join(dataset_path,"{}.pkl".format(args.scaler_name))
+        # dump(scaler, open(scaler_path, 'wb'))     
+        # #Normalize
+        # x = df[cols].values
+        # x_scaled = scaler.fit_transform(x)
+        # df = pd.concat((df["time"],pd.DataFrame(x_scaled,columns=cols)),axis=1)
--- BehaviourClustering/process_raw_acc.py
+++ BehaviourClustering/process_raw_acc.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import os
 import argparse
-
+import sys
+project_path=os.path.dirname(os.getcwd()) 
+sys.path.append(os.path.join(project_path,"Code"))
 from tqdm import tqdm
 from acc_ut import preprocess_accdata
 
@@ -13,11 +15,28 @@ parser.add_argument("--main_dir", type=str,
                     help="Base directory of project")
 
 
+parser.add_argument("-wd","--window_duration", type=int,
+                    help="Duration of sliding window interval in secs")
+                    
+parser.add_argument("-sr","--sample_rate", type=int,
+                    help="Time between samples of accelerometer in msecs")
+                    
+parser.add_argument("-d", "--default", action="store_true",
+                    help="default values for window_duration (2 secs) and sample rate(10 msecs)")
+
+
 if __name__ == "__main__":
     
     # Sample usage :
-    #python process_raw_acc.py --main_dir "C:\Users\jeuux\Desktop\Carrera\MoAI\TFM"
+    #python process_raw_acc.py --main_dir "C:\Users\jeuux\Desktop\Carrera\MoAI\TFM" -d
     args = parser.parse_args()
+    if args.default==True:
+        window_duration=2
+        sample_rate=10
+
+    elif args.default==False:
+        window_duration=args.window_duration
+        sample_rate=args.sample_rate
     acc_path=os.path.join(args.main_dir,"AnnotatedData","Accelerometer_Data")
     participant_path=os.path.join(acc_path,"Participants")
 
@@ -27,14 +46,14 @@ if __name__ == "__main__":
     #open raw tables to get raw acc values and participant info
     for folder in tqdm(participant_folders):
         participant=os.path.basename(folder)
-        file=os.path.join(folder,"acc_raw{}.csv".format(participant))
+        file=os.path.join(folder,"acc_raw_{}.csv".format(participant))
         #convert raw acc data into standard form (remove NaN values, fix frequency)
-        df=preprocess_accdata(pd.read_csv(file))
+        df=preprocess_accdata(pd.read_csv(file),window_duration,sample_rate)
         #save processed df of each participant into its correspondent folder
         dir_path=os.path.join(os.path.dirname(file),"acc_data_{}.csv".format(participant))
         df.to_csv(dir_path,index=False)
 
-    print("Succesfully raw data processed")
+        print("Succesfully raw data  of participant : {} processed".format(participant))
     pass
 
 
