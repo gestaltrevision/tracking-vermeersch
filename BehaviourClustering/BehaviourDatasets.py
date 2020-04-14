7dd56abc03443cbca379b95d21ff7fcbe6b8d2f0
jeux19 <jesgarram19@gmail.com>
2020-04-14 20:16:23 +0200

    File with Torch customs dataset for feading data into DL models

---
+++ BehaviourClustering/BehaviourDatasets.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import os
+import numpy as np
+from torch.utils.data import Dataset,DataLoader
+import joblib
+
+
+class IMUDataset(Dataset):
+    """
+    Dataset class for Loading IMU signals(Accelerometer + Gyroscope) over different sliding windows
+    input:
+        folder: path to main dir of dataset (root containing Train and Test Folder)
+        scaler: type of scaler (eg. StandardScaler) to be applied to data
+            if we are loading the training set(train=True) 
+                we define the scaler over training set and we save it
+                
+            When loading test set, we ommit this value and fit the previously fit scaler over training set
+    output:
+        samples (n_samples,sequence_length,n_components):
+            Array containing the readings of each component over a number of different windows(n_samples)
+        targets (n_samples)
+            Array containing the target (Binary) over each of the correspondant samples
+        
+    """
+    def __init__(self,folder,scaler,train=True):
+        #Get folder ("Train" or "Test")
+        data_type= "Train" if train else "Test"
+        self.data_folder=os.path.join(folder,data_type)
+        #Load data    
+        self.targets=np.load(os.path.join(self.data_folder,"targets.npy"))
+        self.data=np.load(os.path.join(self.data_folder,"samples.npy"))
+        #Reshape as (readings/component,components) to scale data
+        samples,sequence_length,n_components=self.data.shape
+        self.data=self.data.reshape(-1,n_components)
+        
+        #Scalling
+        if(train):
+          self.scaler=scaler.fit(self.data)
+          #save scaler (in dataset folder)
+          joblib.dump(self.scaler,os.path.join(folder,'scaler_train.pkl'))
+        else:
+          #get scaler file
+          scaler_file=next(file for file in os.listdir(folder) if "scaler" in file)
+          self.scaler=joblib.load(os.path.join(folder,scaler_file))
+        #Scale data
+        self.data=self.scaler.transform(self.data).reshape(samples,sequence_length,n_components)
+        
+    def __len__(self):
+        return len(self.targets)
+
+    def __getitem__(self, idx):
+        if isinstance(idx, torch.Tensor):
+                idx = idx.tolist()
+        return [self.data[idx], self.targets[idx]]
