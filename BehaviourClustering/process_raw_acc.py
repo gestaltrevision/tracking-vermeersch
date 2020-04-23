b0afeefb183712e509e0efd719ded98b6c240e43
jeux19 <jesgarram19@gmail.com>
2020-04-23 12:28:53 +0200

    fixed minor bugs

--- BehaviourClustering/process_raw_acc.py
+++ BehaviourClustering/process_raw_acc.py
@@ -6,7 +6,7 @@ import sys
 project_path=os.path.dirname(os.getcwd()) 
 sys.path.append(os.path.join(project_path,"Code"))
 from tqdm import tqdm
-from acc_ut import process_readings_participant,get_events,get_events_index,create_event_col,segment_signal
+from acc_ut import process_readings_participant,segment_signal
 
 
 parser = argparse.ArgumentParser()
@@ -21,23 +21,23 @@ parser.add_argument("-bn","--base_name", type=str,
 parser.add_argument("--dataset_name" ,type=str,
                     help="name of the current version of dataset")
                     
-parser.add_argument("-ns","--n_samples", nargs='?', const=200, type=int, default=200, 
+parser.add_argument("-ns","--n_samples", nargs='?', const=100, type=int, default=100, 
                     help="Number of samples in each sliding window")
 parser.add_argument("-np","--n_points", nargs='?', const=50, type=int, default=50, 
                     help=" Data points in each sliding window after downsampling")
 
 
-parser.add_argument('-fc','--cut_freq', nargs='?', const=10, type=int, default=10,
-                    help="Cut-off frequency of filter")
+
 if __name__ == "__main__":
     # Sample usage :
-    #python process_raw_acc.py --main_dir "C:\Users\jeuux\Desktop\Carrera\MoAI\TFM" --data_type Acc_Gyros --base_name Final_data --dataset_name IMU_dataset
+    #python process_raw_acc.py --main_dir "C:\Users\jeuux\Desktop\Carrera\MoAI\TFM" --data_type CompleteData --base_name Final_data --dataset_name HAR_Dataset
     #
     args=parser.parse_args()
     acc_path=os.path.join(args.main_dir,"AnnotatedData","Accelerometer_Data")
     participant_path=os.path.join(acc_path,"Participants")
     participant_folders=[os.path.join(participant_path,folder) for folder in os.listdir(participant_path)]
     new_samples_number=args.n_points
+    samples_per_sl=args.n_samples
 
     #open raw tables to get raw acc values and participant info
     for participant_folder in tqdm(participant_folders):
@@ -46,8 +46,9 @@ if __name__ == "__main__":
         file=os.path.join(folder,"{0}_raw_{1}.csv".format(args.data_type,participant))
         
         #convert raw acc data into standard form
-        df=process_readings_participant(pd.read_csv(file),participant,args.cut_freq,args.n_samples)
-        sensor_comp,labels=segment_signal(df,new_samples_number,window_size=250)
+        raw_df=pd.read_csv(file,decimal=',')
+        df=process_readings_participant(raw_df,participant,args.n_samples)
+        sensor_comp,labels=segment_signal(df,new_samples_number,samples_per_sl)
 
         np.save(os.path.join(os.path.dirname(file),'segments_data'), sensor_comp)
         np.save(os.path.join(os.path.dirname(file),'targets_data'), labels)
